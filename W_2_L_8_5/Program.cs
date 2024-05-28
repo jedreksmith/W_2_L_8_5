3f6199e43aa3555b0619bb9d7b4ca77f8f95429e
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-28 22:15:45 +0200

    Add project files.

---
+++ W_2_L_8_5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace W_2_L_8_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trzecie potęgi");
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Trzecia potęga liczby " + i + " to " + i * i * i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
